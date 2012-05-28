858c878b99e56e9a9801fc9715333d16cc1d049e
hiraditya <hiraditya@d69eb80e-660b-f9d4-7be1-ad7134eeed8e>
2012-05-28 15:32:50 +0000

    added the function to list utils and modified others

--- trunk/general_utilities/vector_utils.hpp
+++ trunk/general_utilities/vector_utils.hpp
@@ -4,14 +4,28 @@
 #include<vector>
 #include<iostream>
 namespace general_utilities{
-  template<typename T>
-  std::ostream& operator<<(std::ostream& os,const std::vector<T>& v)
-  {
-    for(typename std::vector<T>::const_iterator i = v.begin(); i!= v.end(); ++i){
-      os<<*i;
-      os<<"\n";
+  // A helper function to simplify the main part.
+    template<typename T>
+    std::ostream& operator<<(std::ostream& os,const std::vector<T>& v)
+    {
+      for(typename std::vector<T>::const_iterator i = v.begin(); i!= v.end(); ++i){
+        os<<*i;
+        os<<"\n";
+      }
+      return os;
     }
-    return os;
+
+#ifdef USE_ITERATORS
+#include<iterator>
+
+  template<class T>
+  std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
+  {
+      std::copy(v.begin(), v.end(), std::ostream_iterator<T>(os, "\n"));
+      return os;
   }
+
+#endif
+
 }
 #endif // UTILS_VECTOR_HPP
