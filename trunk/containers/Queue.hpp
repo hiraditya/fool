df0423e7c3dd8f67ebd18341708962a50ccce53b
hiraditya <hiraditya@d69eb80e-660b-f9d4-7be1-ad7134eeed8e>
2012-05-29 22:13:03 +0000

    small corrections

--- trunk/containers/Queue.hpp
+++ trunk/containers/Queue.hpp
@@ -4,7 +4,7 @@
 /*
  *  @author Aditya Kumar 
  *
- *  This file is distributed under the MIelement_type License. See 
+ *  This file is distributed under the MIT License. See 
  *  LICENCE.txt attached with this project or visit
  *  http://www.opensource.org/licenses/mit-license.php 
  *  for terms and conditions.
