a558bdc78675d28700762b2d5b9d4526d6a3a554
hiraditya <hiraditya@d69eb80e-660b-f9d4-7be1-ad7134eeed8e>
2012-09-04 17:11:38 +0000

    added a script that can be used to note the time when a process ends

---
+++ trunk/general_utilities/process_poll.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import datetime
+#import subprocess
+import commands
+import time
+import sys
+
+def main(argv):
+  if len(argv) < 2:
+    sys.stderr.write("Usage: %s <action>" % (argv[0],))
+    return 1
+  output = commands.getoutput("ps -e")
+  while argv[1] in output:
+    output = commands.getoutput("ps -e")
+    print argv[1], " running", datetime.datetime.now() 
+    #print output
+    time.sleep(30)
+  sys.stderr.write("Process finished at %s\n" % datetime.datetime.now())
+
+if __name__ == "__main__":
+  sys.exit(main(sys.argv))
