199523f72ba07400f7ab37ffcb7df79e9b5164bf
hiraditya <hiraditya@d69eb80e-660b-f9d4-7be1-ad7134eeed8e>
2013-01-20 02:43:43 +0000

    test cases for functionals

---
+++ trunk/test/test_hof.cpp
@@ -0,0 +1,19 @@
+#include <fool/standard/hof.hpp>
+#include<iostream>
+#include<vector>
+
+using namespace fool::standard;
+int main()
+{
+  std::vector<int> vi{1,2,3,4};
+  auto f = [](int i){ return i+2; };
+  std::vector<int> vo = FMap (f,vi);
+  for(auto i = vi.begin(), o = vo.begin(); 
+      i != vi.end(); ++i, ++o)
+  {
+    std::cout<<*i<<"\t";
+    std::cout<<*o<<"\n";
+  }
+  return 0;
+}
+
