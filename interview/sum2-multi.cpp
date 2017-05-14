2870cd0de0d9b8abdd120b3780de8201f268803a
hiraditya <hiraditya@gmail.com>
2017-05-14 11:56:15 -0500

    sum2 and sum2-multi

---
+++ interview/sum2-multi.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<array>
+
+/**
+   To find two numbers in an array whose sum is zero.
+ */
+
+// Returns the positions which sum to zero.
+template<typename T>
+// [b, e)
+std::pair<T, T> find_sum2(T b, T e) {
+  T p1 = b;
+  T p2 = e;
+  --p2; // Point to the last element of the range.
+  while (p1 != p2) {
+    int sum = *p1 + *p2;
+    if (sum == 0)
+      return {p1, p2};
+    else if (sum < 0)
+      ++p1;
+    else // sum > 0
+      --p2;
+  }
+  return {nullptr, nullptr};
+}
+
+int main() {
+  // Sorted array.
+  std::array<int, 9> a{ -4, -4, -1, 0, 1, 2, 3, 4, 4 };
+  std::pair<int*, int*> v = find_sum2(a.begin(), a.end());
+  auto b = a.begin();
+
+  while (v.first != nullptr) {
+    std::cout << "\nFound: " << *v.first << ", " << *v.second;
+    std::cout << " Position: " << v.first -b << ", " << v.second -b;
+    v = find_sum2(++v.first, v.second);
+  }
+  return 0;
+}
