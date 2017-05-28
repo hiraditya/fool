3157041d37f91e5e94c63e201e7282536a54626e
hiraditya <hiraditya@gmail.com>
2017-05-28 11:35:17 -0500

    format

--- interview/num2string.cpp
+++ interview/num2string.cpp
@@ -1,45 +1,41 @@
-#include<cstdio>
+#include <cstdio>
 
-void num2str(int n) {
-char buffer[50];
-int i = 0;
+void num2str(int n, char *buffer) {
+  int i = 0;
 
-bool isNeg = n<0;
+  bool isNeg = n < 0;
+  unsigned int n1 = isNeg ? -n : n;
 
-unsigned int n1 = isNeg ? -n : n;
+  while (n1 != 0) {
+    buffer[i++] = n1 % 10 + '0';
+    n1 = n1 / 10;
+  }
 
-while(n1!=0)
-{
-    buffer[i++] = n1%10+'0';
-    n1=n1/10;
-}
-
-if(isNeg)
+  if (isNeg)
     buffer[i++] = '-';
 
-buffer[i] = '\0';
+  buffer[i] = '\0';
 
-for(int t = 0; t < i/2; t++)
-{
-    buffer[t] ^= buffer[i-t-1];
-    buffer[i-t-1] ^= buffer[t];
-    buffer[t] ^= buffer[i-t-1];
-}
+  // reverse the string
+  for (int t = 0; t < i / 2; t++) {
+    buffer[t] ^= buffer[i - t - 1];
+    buffer[i - t - 1] ^= buffer[t];
+    buffer[t] ^= buffer[i - t - 1];
+  }
 
-if(n == 0)
-{
+  if (n == 0) {
     buffer[0] = '0';
     buffer[1] = '\0';
-}   
+  }
 
-printf("%s\n", buffer);
+  printf("%s\n", buffer);
 }
 
 int main() {
-	// your code goes here
-	num2str(-1);
-	num2str(-0);
-	num2str(100);
-	num2str(-100);
-	return 0;
+  char buf[50];
+  num2str(-1, buf);
+  num2str(-0, buf);
+  num2str(100, buf);
+  num2str(-100, buf);
+  return 0;
 }
