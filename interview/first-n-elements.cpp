0101cfa55b464c1000153781080165b00f9ab8f9
hiraditya <hiraditya@gmail.com>
2017-06-27 23:11:04 -0500

    using the file from ifstream

--- interview/first-n-elements.cpp
+++ interview/first-n-elements.cpp
@@ -1,13 +1,31 @@
-#include<queue>
+#include <queue>
 #include <iostream>
+#include <fstream>
 #include <chrono>
+#include <thread>
 
 /* Very simply question of inserting latest 5000 records in
    container along with timestamp and retrieve them as needed and discard which are older then 5000. 
 */
 
-struct Record {
+typedef std::chrono::time_point<std::chrono::system_clock> Time;
+
+class Record {
+  int i;
+public:
+  friend std::istream& operator>>(std::istream &is, Record &r);
+  friend std::ostream& operator<<(std::ostream &os, const Record &r);
 };
+
+std::istream& operator>>(std::istream &is, Record &r) {
+  is >> r.i;
+  return is;
+}
+
+std::ostream& operator<<(std::ostream &os, const Record &r) {
+  os << " " << r.i;
+  return os;
+}
 
 typedef std::pair<Record, Time> Data;
 
@@ -17,10 +35,14 @@ struct MaintainRecords {
 
   MaintainRecords(int m) : Max(m) {}
 
+  std::queue<Data>& get_queue() {
+    return q;
+  }
+
   void insert(Data d) {
     if (q.size() == Max)
-      q.pop_front();
-    q.push_back(d);
+      q.pop();
+    q.push(d);
   }
 
   void insert(Record r, Time t) {
@@ -30,10 +52,20 @@ struct MaintainRecords {
 
 int main() {
   MaintainRecords m(5000);
+  std::ifstream data("elements.txt");
   Record r;
   while(data >> r) {
-    std::chrono::time_point<std::chrono::system_clock> t = std::chrono::system_clock::now();
-    insert(r, t);
-  } 
+    Time t = std::chrono::system_clock::now();
+    m.insert(r, t);
+    std::this_thread::sleep_for(std::chrono::microseconds(10));
+  }
+  auto &q = m.get_queue();
+  while (!q.empty()) {
+    auto &rec = q.front();
+    //std::time_t now_c = std::chrono::system_clock::to_time_t(rec.second);
+    std::cout << rec.first; // << ", time: " << now_c << "\n";
+    q.pop();
+  }
+  return 0;
 }
 
