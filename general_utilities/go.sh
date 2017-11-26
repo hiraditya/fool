335b8fd2ffd1e05bb5f97366cc71f603b83f841e
Aditya Kumar <hiraditya@msn.com>
2017-11-26 10:58:00 -0800

    general utilities for node.js on ec2

---
+++ general_utilities/go.sh
@@ -0,0 +1,7 @@
+#curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
+
+#ssh -i ~/.ssh/kp-one.pem ubuntu@ec2-54-213-1-2.us-west-2.compute.amazonaws.com
