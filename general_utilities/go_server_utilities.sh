5d1f8fe483fdd5088c5690c9b690c4fbdba8501f
Aditya Kumar <hiraditya@msn.com>
2018-01-07 13:58:45 -0800

    redirect utils

--- general_utilities/go_server_utilities.sh
+++ general_utilities/go_server_utilities.sh
@@ -14,13 +14,16 @@ sudo apt-get install -y build-essential
 redirect() {
   #How to redirect port 80 request to port 3000 https://stackoverflow.com/questions/16573668/best-practices-when-running-node-js-with-port-80-ubuntu-linode
 
+  # removing iptable entries
+  #http://lubos.rendek.org/remove-all-iptables-prerouting-nat-rules/
+
   # to redirect a port to another port
   sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
   # to list all prerouting maps
   sudo iptables -t nat --line-numbers -L
   # the list of prerouting redirects look like this
   Chain PREROUTING (policy ACCEPT)
-  num  target     prot opt source               destination         
+  num  target     prot opt source               destination
   1    REDIRECT   tcp  --  anywhere             anywhere             tcp dpt:http redir ports 3000
 
   # to remove a prerouting entry num
@@ -40,3 +43,29 @@ forever start -c "npm start" ./
 forever list
 # ssh to amazon aws or ec2
 ssh -i ~/.ssh/kp-one.pem ubuntu@xxxxxx.compute.amazonaws.com
+
+
+
+nginx_for_multiple_proxy() {
+  http://randomdotnext.com/host-multiple-sites-on-single-ec2-instance-nginx-node/
+
+  sudo vi /etc/nginx/sites-available/default
+# make entries like this, you can make multiple entries one for each domain name
+# --------------------------------------------
+server {
+    listen 80;
+    server_name example1.com; # domain
+    location / {
+        proxy_pass http://localhost:3030; # port
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+    }
+}
+# --------------------------------------------
+# after that reload nginx
+  sudo nginx -s reload
+}
+
