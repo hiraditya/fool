19a93e72ff5445e25f1236613808b0ae8b79dfd4
Aditya Kumar <hiraditya@msn.com>
2018-02-04 12:40:39 -0800

    parse html files

--- general_utilities/html_parser.py
+++ general_utilities/html_parser.py
@@ -87,4 +87,77 @@ def parse_basi():
     print(']')
 
 
-parse_basi()
+#parse_basi()
+
+def print_csv(t, delim, title):
+    t1 = t.strip().split(delim)
+    t1strip = [x.strip() for x in t1]
+    t1quoted = ['"'+x+'",' for x in t1strip]
+    #t1strip = map(str.strip, t1)
+    print (title+':[\n  ', end="")
+    print('\n  '.join(t1quoted))
+    print('],')
+
+
+'''
+print title from filename by removing .html
+'''
+def print_title(t):
+    # remove the .html from file name
+    print("title:'" + t[:-5] + "',")
+
+'''
+parse simple html
+'''
+def parse_simple_html(dirname, filenames):
+    rn = 26
+    try:
+        from BeautifulSoup import BeautifulSoup
+    except ImportError:
+        from bs4 import BeautifulSoup
+        for f in filenames:
+            if (is_html(f)):
+                html = open(os.path.join(dirname, f)) #the HTML code you've written above
+                parsed_html = BeautifulSoup(html, "lxml")
+                h2 = parsed_html.body.find('i')
+                p = parsed_html.body.find('p')
+                if h2 is not None:
+                    print('{')
+                    print('id:' + str(rn) + ',')
+                    rn = rn + 1
+                    # remove the .html from file name
+                    print_title(f)
+                    print('image:"../images/",')
+                    print_csv(h2.text, ',', 'ingredients')
+                    print_csv(p.text, '\n', 'steps')
+                    print('active_time:"half hours",')
+                    print('total_time:"half hours",')
+                    print('yield: 2')
+                    print('},')
+
+                html.close()
+
+
+def print_json_begin(m):
+    print('module.exports.' + m + ' = [')
+
+def print_json_end():
+    print(']')
+
+def parse_html_files():
+    f = [
+    'falafel.html',
+    'eggcucumber_salad.html',
+    'banana_cake.html',
+    'avocado-rice.html',
+    'scrambled_egg.html',
+    'patisapta.html',
+    'parmesanrice.html',
+    'khichuri.html'
+    ]
+    d = '/home/hiraditya/work/basikhobor-docs/recipe/'
+    print_json_begin('recipe')
+    parse_simple_html(d, f)
+    print_json_end()
+
+parse_html_files()
