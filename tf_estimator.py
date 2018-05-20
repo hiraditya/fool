095953c18bbb9f91d3643a1e1ec74f029c961703
Aditya Kumar <hiraditya@gmail.com>
2018-05-19 19:21:27 -0700

    Update tf_estimator.py

--- tf_estimator.py
+++ tf_estimator.py
@@ -122,6 +122,8 @@ df = bq.Query(query).to_dataframe()
 
 
 # TF Challenge
-# Create a neural network that is capable of finding the volume of a cylinder given the radius of its base (r) and its height (h). Assume that the radius and height of the cylinder are both in the range 0.5 to 2.0. Simulate the necessary training dataset.
+'''
+Create a neural network that is capable of finding the volume of a cylinder given the radius of its base (r) and its height (h). Assume that the radius and height of the cylinder are both in the range 0.5 to 2.0. Simulate the necessary training dataset.
 
 The input features will be r and h and the label will be  πr2hπr2h  Create random values for r and h and compute V. Your dataset will consist of r, h and V. Then, use a DNN regressor. Make sure to generate enough data.
+'''
