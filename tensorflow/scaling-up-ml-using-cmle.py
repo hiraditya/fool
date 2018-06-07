e73d54f65c7dc7d236850d93c817eff2653ab16d
Aditya Kumar <hiraditya@gmail.com>
2018-06-07 12:25:10 -0700

    Update scaling-up-ml-using-cmle.py

--- tensorflow/scaling-up-ml-using-cmle.py
+++ tensorflow/scaling-up-ml-using-cmle.py
@@ -109,3 +109,63 @@ gcloud ml-engine local train \
    --eval_data_paths=${PWD}/taxi-valid.csv  \
    --train_steps=1000 \
    --output_dir=${PWD}/taxi_trained
+
+'''
+When I ran it (due to random seeds, your results will be different), the average_loss (Mean Squared Error) on the evaluation dataset was 187, meaning that the RMSE was around 13.
+'''
+from google.datalab.ml import TensorBoard
+TensorBoard().start('./taxi_trained')
+for pid in TensorBoard.list()['pid']:
+  TensorBoard().stop(pid)
+  print 'Stopped TensorBoard with pid {}'.format(pid)
+
+'''
+    If the above step (to stop TensorBoard) appears stalled, just move on to the next step. You don't need to wait for it to return.
+'''
+!ls $PWD/taxi_trained
+
+'''
+ Submit training job using gcloud
+
+First copy the training data to the cloud. Then, launch a training job.
+
+After you submit the job, go to the cloud console (http://console.cloud.google.com) and select Machine Learning | Jobs to monitor progress.
+
+Note: Don't be concerned if the notebook stalls (with a blue progress bar) or returns with an error about being unable to refresh auth tokens. This is a long-lived Cloud job and work is going on in the cloud. Use the Cloud Console link (above) to monitor the job.
+https://cloud.google.com/ml-engine/docs/tensorflow/getting-started-training-prediction
+'''
+
+%bash
+echo $BUCKET
+gsutil -m rm -rf gs://${BUCKET}/taxifare/smallinput/
+gsutil -m cp ${PWD}/*.csv gs://${BUCKET}/taxifare/smallinput/
+
+
+%%bash
+OUTDIR=gs://${BUCKET}/taxifare/smallinput/taxi_trained
+JOBNAME=lab3a_$(date -u +%y%m%d_%H%M%S)
+echo $OUTDIR $REGION $JOBNAME
+gsutil -m rm -rf $OUTDIR
+gcloud ml-engine jobs submit training $JOBNAME \
+   --region=$REGION \
+   --module-name=trainer.task \
+   --package-path=${PWD}/taxifare/trainer \
+   --job-dir=$OUTDIR \
+   --staging-bucket=gs://$BUCKET \
+   --scale-tier=BASIC \
+   --runtime-version=$TFVERSION \
+   -- \
+   --train_data_paths="gs://${BUCKET}/taxifare/smallinput/taxi-train*" \
+   --eval_data_paths="gs://${BUCKET}/taxifare/smallinput/taxi-valid*"  \
+   --output_dir=$OUTDIR \
+   --train_steps=10000
+
+Job [lab3a_180607_192245] submitted successfully.
+Your job is still active. You may view the status of your job with the command
+
+  $ gcloud ml-engine jobs describe lab3a_180607_192245
+
+or continue streaming the logs with the command
+
+  $ gcloud ml-engine jobs stream-logs lab3a_180607_192245
+Use the Cloud Console link to monitor the job and do NOT proceed until the job is done.
