dcfb85277affafa7242c8e48d6c18d40ec4b411f
Aditya Kumar <hiraditya@gmail.com>
2018-06-06 21:34:19 -0700

    Update tf-distributed-training-and-monitoring.py

--- tensorflow/tf-distributed-training-and-monitoring.py
+++ tensorflow/tf-distributed-training-and-monitoring.py
@@ -61,3 +61,65 @@ def get_valid():
 
 def get_test():
   return read_dataset('./taxi-test.csv', mode = tf.estimator.ModeKeys.EVAL)
+
+'''
+ Create features out of input data
+
+For now, pass these through. (same as previous lab)
+'''
+
+INPUT_COLUMNS = [
+    tf.feature_column.numeric_column('pickuplon'),
+    tf.feature_column.numeric_column('pickuplat'),
+    tf.feature_column.numeric_column('dropofflat'),
+    tf.feature_column.numeric_column('dropofflon'),
+    tf.feature_column.numeric_column('passengers'),
+]
+
+def add_more_features(feats):
+  # Nothing to add (yet!)
+  return feats
+
+feature_cols = add_more_features(INPUT_COLUMNS)
+
+'''
+Serving input function
+'''
+# Defines the expected shape of the JSON feed that the model
+# will receive once deployed behind a REST API in production.
+def serving_input_fn():
+  feature_placeholders = {
+    'pickuplon' : tf.placeholder(tf.float32, [None]),
+    'pickuplat' : tf.placeholder(tf.float32, [None]),
+    'dropofflat' : tf.placeholder(tf.float32, [None]),
+    'dropofflon' : tf.placeholder(tf.float32, [None]),
+    'passengers' : tf.placeholder(tf.float32, [None]),
+  }
+  # You can transforma data here from the input format to the format expected by your model.
+  features = feature_placeholders # no transformation needed
+  return tf.estimator.export.ServingInputReceiver(features, feature_placeholders)
+
+'''
+tf.estimator.train_and_evaluate
+'''
+def train_and_evaluate(output_dir, num_train_steps):
+  estimator = tf.estimator.LinearRegressor(
+                       model_dir = output_dir,
+                       feature_columns = feature_cols)
+  train_spec=tf.estimator.TrainSpec(
+                       input_fn = read_dataset('./taxi-train.csv', mode = tf.estimator.ModeKeys.TRAIN),
+                       max_steps = num_train_steps)
+  exporter = tf.estimator.LatestExporter('exporter', serving_input_fn)
+  eval_spec=tf.estimator.EvalSpec(
+                       input_fn = read_dataset('./taxi-valid.csv', mode = tf.estimator.ModeKeys.EVAL),
+                       steps = None,
+                       start_delay_secs = 1, # start evaluating after N seconds
+                       throttle_secs = 10,  # evaluate every N seconds
+                       exporters = exporter)
+  tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+
+'''
+ Monitoring with TensorBoard
+
+Use "refresh" in Tensorboard during training to see progress.
+'''
