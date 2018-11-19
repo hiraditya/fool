3552b94dd8231f52defe20f2d7e37b7c8dd842c4
Aditya Kumar <hiraditya@gmail.com>
2018-11-19 05:34:09 -0800

    Update handtuning.py

--- tensorflow/handtuning.py
+++ tensorflow/handtuning.py
@@ -71,4 +71,21 @@ def train_and_evaluate(output_dir, num_train_steps):
 
 # Run training    
 shutil.rmtree(OUTDIR, ignore_errors = True) # start fresh each time
-train_and_evaluate(OUTDIR, num_train_steps = 500) 
+train_and_evaluate(OUTDIR, num_train_steps = 500)
+
+
+'''
+Is there a standard method for tuning the model?
+This is a commonly asked question. The short answer is that the effects of different hyperparameters is data dependent. So there are no hard and fast rules; you'll need to run tests on your data.
+
+Here are a few rules of thumb that may help guide you:
+
+Training error should steadily decrease, steeply at first, and should eventually plateau as training converges.
+If the training has not converged, try running it for longer.
+If the training error decreases too slowly, increasing the learning rate may help it decrease faster.
+But sometimes the exact opposite may happen if the learning rate is too high.
+If the training error varies wildly, try decreasing the learning rate.
+Lower learning rate plus larger number of steps or larger batch size is often a good combination.
+Very small batch sizes can also cause instability. First try larger values like 100 or 1000, and decrease until you see degradation.
+Again, never go strictly by these rules of thumb, because the effects are data dependent. Always experiment and verify.
+'''
